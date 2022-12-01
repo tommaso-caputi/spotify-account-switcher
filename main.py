dd3261e42831a6db1b92b5f954ff8f49cacaf837
tommaso caputi <tommasocaputi85@gmail.com>
2022-12-01 22:49:53 +0100

    final functions

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ import time
 import tkinter
 import customtkinter
 
-
 load_dotenv()
 ACC1 = os.getenv("ACC1")
 PASS1 = os.getenv("PASS1")
 ACC2 = os.getenv("ACC2")
 PASS2 = os.getenv("PASS2")
+
+customtkinter.set_appearance_mode("System")  
+customtkinter.set_default_color_theme("blue")  
+app = customtkinter.CTk()  
+app.geometry("400x240")
+app.title("Choose account")
+app.eval("tk::PlaceWindow . center")
+button = customtkinter.CTkButton(master=app, text=ACC1, command=lambda: check_account(ACC1, PASS1))
+button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+button = customtkinter.CTkButton(master=app, text=ACC2, command=lambda: check_account(ACC2, PASS2))
+button.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
+
 
 def get_actual_account():
     file = open(os.getenv("ACTUAL_ACCOUNT_PATH"))
@@ -19,12 +30,17 @@ def get_actual_account():
     return acc
 
 def set_actual_account(acc):
+    global app
     file = open(os.getenv("ACTUAL_ACCOUNT_PATH"), "w")
     file.write(acc)
     file.close() 
+    app.destroy()
 
-def open_spotify():
-    os.startfile(os.getenv("SPOTIFY_PATH"))
+def check_account(acc, passwd):
+    if acc != get_actual_account():
+        os.startfile(os.getenv("SPOTIFY_PATH"))
+        time.sleep(3)
+        change_account(acc, passwd)
 
 def change_account(acc, passwd):
     time.sleep(0.3)
@@ -34,8 +50,8 @@ def change_account(acc, passwd):
     pyautogui.click()
     pyautogui.moveTo(960, 350)  #position of username
     pyautogui.click()
-    pyautogui.press('backspace', presses=20)
-    time.sleep(0.1)
+    pyautogui.press('backspace', presses=30)
+    time.sleep(0.5)
     pyautogui.write(acc)
     pyautogui.moveTo(960, 420)  #position of password
     pyautogui.click()
@@ -50,14 +66,4 @@ def button_function():
     print("button pressed")
 
 if __name__ == "__main__":
-    customtkinter.set_appearance_mode("System")  
-    customtkinter.set_default_color_theme("blue")  
-    app = customtkinter.CTk()  
-    app.geometry("400x240")
-    app.title("Choose account")
-    app.eval("tk::PlaceWindow . center")
-    button = customtkinter.CTkButton(master=app, text=ACC1, command=button_function)
-    button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-    button = customtkinter.CTkButton(master=app, text=ACC2, command=button_function)
-    button.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
     app.mainloop()
