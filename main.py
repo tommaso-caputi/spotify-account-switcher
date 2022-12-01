ee49d5be40b27687e7235122168284402a1bb49e
tommaso caputi <tommasocaputi85@gmail.com>
2022-12-01 21:11:36 +0100

    ,

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-print(pyautogui.size())
-
 ACC1 = os.getenv("ACC1")
 PASS1 = os.getenv("PASS1")
 
-print(ACC1, PASS1)
+
+pyautogui.moveTo(1650, 30)  #position of account button
